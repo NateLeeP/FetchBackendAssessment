8c5a5332ef4dea77d2b937e49dc9fe00a3044ea3
NateLeeP <nlpruitt@gmail.com>
2021-02-24 18:39:32 -0500

    Minor syntax fixes

--- routes/transactionGetRoute.js
+++ routes/transactionGetRoute.js
@@ -3,3 +3,4 @@ const transactions = require('./memory.js');
 module.exports = (req, res) => {
   res.status(200).send(transactions);
 }
+
--- routes/transactionPostRoute.js
+++ routes/transactionPostRoute.js
@@ -3,9 +3,9 @@ const transactions = require('./memory.js');
 
 
 module.exports = (req, res) => {
-  var {payer, points, date} = req.body;
-
-  var newTransactions = {payer, points, date}
+  var {payer, points, date, time} = req.body;
+  var timestamp = date + 'T' + time + ':00Z'
+  var newTransactions = {payer, points, timestamp: new Date(timestamp)}
   transactions.push(newTransactions);
 
   res.status(201).send('Ok')
