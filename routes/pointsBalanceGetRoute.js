51a6e5588d11b54b26addb494c3ee156981cf2cb
NateLeeP <nlpruitt@gmail.com>
2021-02-24 18:40:01 -0500

    Route to return current point balances for payers

---
+++ routes/pointsBalanceGetRoute.js
@@ -0,0 +1,19 @@
+const transactions = require('./memory.js');
+
+module.exports = (req, res) => {
+  // function to return blaances for payers
+
+  // point balance object
+  balances = {};
+
+  // loop through transactions, add points
+  for (var i = 0; i < transactions.length; i++) {
+    if (balances[transactions[i]['payer']]) {
+      balances[transactions[i]['payer']] += transactions[i]['points']
+    } else {
+      balances[transactions[i]['payer']] = transactions[i]['points']
+    }
+  }
+
+  res.status(200).send(balances)
+}
