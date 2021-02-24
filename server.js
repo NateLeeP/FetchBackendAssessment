9fff0b00b4e47cdf024f62c7f210ac377ee9cdd7
NateLeeP <nlpruitt@gmail.com>
2021-02-24 14:09:48 -0500

    Initial server.js commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const router = require('./routes/router.js');
+const PORT = 3000
+
+// add a transaction
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}!`)
+})
