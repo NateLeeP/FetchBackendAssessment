f21284becee19584515e7b595ecf42291800b381
NateLeeP <nlpruitt@gmail.com>
2021-02-25 12:09:16 -0500

    Remove default memory from server.

--- routes/memory.js
+++ routes/memory.js
@@ -1,13 +1,3 @@
 // In-memory data
-
-  /*
-  module.exports = [
-    {"payer": "DANNON", "points":1000, "timestamp": new Date("2020-11-02T14:00:00Z")},
-    {"payer":"UNILEVER", "points": 200, "timestamp": new Date("2020-10-31T11:00:00Z")},
-    {"payer":"DANNON", "points": -200, "timestamp": new Date("2020-10-31T15:00:00Z")},
-    {"payer":"MILLER COORS", "points": 10000, "timestamp": new Date("2020-11-01T14:00:00Z")},
-    {"payer": "DANNON", "points":300, "timestamp": new Date("2020-10-31T10:00:00Z")}
-  ];
-  */
 module.exports = [];
 
